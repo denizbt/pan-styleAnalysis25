aa10b1231b77346255ac6c3b2495cec24bb00ca9
denizbt <deniz.boloni.turgut@gmail.com>
2025-04-17 22:35:33 -0400

    Add support for resume_training from saved encoder pth file in bert-cls.py

--- bert-cls.py
+++ bert-cls.py
@@ -1,7 +1,6 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torch import nn
-import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel
 from sklearn.metrics import f1_score, accuracy_score, recall_score, precision_score
 from tqdm import tqdm
@@ -16,8 +15,10 @@ import argparse
 
 def get_args():
   parser = argparse.ArgumentParser()
-  parser.add_argument("--model-name", type=str, default="bert-base-cased")
+  parser.add_argument("--model-name", type=str, default="roberta-base")
   parser.add_argument("--data-dir", type=str, default="data/")
+  parser.add_argument("--workers", type=int, default=2)
+  parser.add_argument("--resume-training", type=str, default="None")
   
   return parser.parse_args()
 
@@ -27,24 +28,30 @@ class BertStyleNN(nn.Module):
   1. Uses self.encoder for independent feature extraction of two sentences.
   2. Concatenates the two embeddings, and passes through StyleNN (defined in mlp.py) for final classification.
   """
-  def __init__(self, hidden_dims=[512, 256, 128, 64], output_dim=1, enc_model_name='bert-base-cased', pooling='mean'):
+  def __init__(self, hidden_dims=[512, 256, 128, 64], output_dim=1, enc_model_name='roberta-base', pooling='mean', resume_training=None):
     """
     Args:
       hidden_dims [List[int]]:
       output_dim [int]: final classification of the model is a single dimension
+      pooling [str] in ['mean', 'cls]
+      resume_training: If not None, contains path to encoder-only model state dict from which to resume training
     """
     super(BertStyleNN, self).__init__()  
-
-    self.encoder = AutoModel.from_pretrained(enc_model_name)
-    self.pooling = pooling
     
+    self.encoder = AutoModel.from_pretrained(enc_model_name)
+    if resume_training is not None:
+      print(f"resuming training from {resume_training}")
+      self.encoder.load_state_dict(torch.load(resume_training))
+    
+    self.pooling = pooling
+
     embedding_dim =  self.encoder.config.hidden_size
     # input to MLP is the concatenation of the sentence pairs extracted
     self.mlp = StyleNN(input_dim=embedding_dim*2, hidden_dims=hidden_dims, output_dim=output_dim)
   
   def forward(self, input_ids1, attention_mask1, input_ids2, attention_mask2):
     # extract features from encoder independently on the two sentences
-    # pooler_output takes [CLS] hidden layer vector
+    # pooler_output takes [CLS] hidden layer vector 
     s1 = self.encoder(input_ids=input_ids1, attention_mask=attention_mask1)
     s2 = self.encoder(input_ids=input_ids2, attention_mask=attention_mask2)
     
@@ -127,10 +134,10 @@ def train(args, train_pairs, train_labels, val_pairs, val_labels, batch_size=16,
   train_set = BertPairDataset(tokenizer, train_pairs, train_labels)
   val_set = BertPairDataset(tokenizer, val_pairs, val_labels)
   
-  train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2, pin_memory=True)
-  val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=2, pin_memory=True)
+  train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=args.workers, pin_memory=True)
+  val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=args.workers, pin_memory=True)
   
-  model = BertStyleNN(enc_model_name=args.model_name)
+  model = BertStyleNN(enc_model_name=args.model_name, resume_training= None if args.resume_training == "None" else args.resume_training)
   model.to(device)
   # scaler = GradScaler()
   print(f"model is on {device}")
@@ -143,7 +150,10 @@ def train(args, train_pairs, train_labels, val_pairs, val_labels, batch_size=16,
   ])
 
   scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
-  criterion = nn.BCEWithLogitsLoss()
+  
+  pos_weight = torch.tensor([0.8 / 0.2])  # ratio of negative to positive
+  pos_weight = pos_weight.to(device)  # move to GPU if using cuda
+  criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
   
   best_val_preds = None
   best_metrics = {'f1': -1}
@@ -170,8 +180,11 @@ def train(args, train_pairs, train_labels, val_pairs, val_labels, batch_size=16,
 
       train_running_loss += loss.item()
     
-    # save model after every epoch
-    torch.save(model.state_dict(), f"{args.model_name}-e{e}.pth")
+    # save model (encoder and entire model) after every epoch
+    file_path = args.model_name.split("/")
+    file_path = file_path[len(file_path)-1]
+    torch.save(model.state_dict(), f"{file_path}-e{e}.pth")
+    torch.save(model.encoder.state_dict(), f"enc-only-{file_path}-e{e}.pth")
     
     avg_train_loss = train_running_loss / len(train_loader)
     metrics, avg_val_loss, val_preds = val(model, val_loader, criterion, device)
@@ -284,11 +297,10 @@ if __name__ == "__main__":
   train_labels = np.load("train_labels.npy")
   val_labels = np.load("val_labels.npy")
   
-  assert len(train_pairs) == len(train_labels)
-  assert len(val_pairs) == len(val_labels)
+  # assert len(train_pairs) == len(train_labels)
+  # assert len(val_pairs) == len(val_labels)
   
   print("read in data.")
   torch.cuda.empty_cache() # to reduce memory problems  
-  # print("cuda?", torch.cuda.is_available())
 
   train(args, train_pairs, train_labels, val_pairs, val_labels)
